67ac3f6e02f1430cd3a30f939549ba3d02ae11f1
mbovel <matthieu@bovel.net>
2015-02-04 17:41:39 +0100

    Remove Network:true
    Didn’t know Network:true means that the plugin cannot be activated on a
    per site basis. I thought it was just to indicate compatibility with
    multisite installations…

--- tinymce-no-resize.php
+++ tinymce-no-resize.php
@@ -4,7 +4,6 @@ Plugin Name: TinyMCE No Resize
 Description: This plugin allows you to turn off the resizing handles on images, tables or media objects in the TinyMCE editor.
 Author: Matthieu Bovel
 Author URI: http://matthieu.bovel.net
-Network: true
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.txt
 Version: 0.1
